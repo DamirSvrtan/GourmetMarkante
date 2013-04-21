8df529ca7d65d72efd1c977533954ea2347d4273
DamirSvrtan <damir.svrtan@gmail.com>
2013-04-21 13:58:28 +0200

    Initial commit with carousel

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+	def kontakt
+	end
+
+	def galerie
+	end
+
+	def speisekarte
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GourmetMarkante::Application.config.secret_token = '7cacc682678791ef514491ea407c1dd981c0d2a53c306727aad46e3e16b8d0d799d2dd227a06a361139a591e64d06d24207087d9760fe1c012146d5be4366dac'
